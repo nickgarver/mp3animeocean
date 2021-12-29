042a8897e8a191303ef12b5c074d83816538ece0
Nick Garver <nickgarver5@gmail.com>
2021-12-28 21:19:34 -0500

    added ko-fi link

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ const App = () => {
   return (
   <div className="App min-h-screen text-blue-200 d-flex align-items-center justify-content-center">
     <img id="poweredby" src={process.env.PUBLIC_URL + '/Poweredby_640px-Black_VertLogo.png'} alt="powered by giphy" />
-    <a id='donate-head' aria-label="Donate" href="https://paypal.me/islandboyluna" target="_blank" rel="noopener noreferrer" className={support ? 'animated bounceOut': 'animated bounceIn'} > <FontAwesomeIcon icon={faHeart} /> Help this server stay alive!<FontAwesomeIcon className="socials-icn close-icn" icon={faTimes} onClick={supportClick} /></a>
+    <a id='donate-head' aria-label="Donate" href="https://ko-fi.com/internetboy" target="_blank" rel="noopener noreferrer" className={support ? 'animated bounceOut': 'animated bounceIn'} > <FontAwesomeIcon icon={faHeart} /> Help this server stay alive!<FontAwesomeIcon className="socials-icn close-icn" icon={faTimes} onClick={supportClick} /></a>
     <Suspense fallback={renderLoader()}>
       <FileUpload id="file-upload" />
     </Suspense>
@@ -31,7 +31,7 @@ const App = () => {
         <a aria-label="Home" href="/"className="site-txt" > MP3 Anime 2021 ©{' '} </a>
         <a aria-label="Twitter" data-tooltip="Report Bug" href="https://twitter.com/internetboysux" target="_blank" rel="noopener noreferrer" ><FontAwesomeIcon className="socials-icn" icon={faTwitter} /></a>
         <a aria-label="Instagram" data-tooltip="Follow me" href="https://instagram.com/internetboy" target="_blank" rel="noopener noreferrer" ><FontAwesomeIcon className="socials-icn" icon={faInstagram} /></a>
-        <a aria-label="Support" data-tooltip="Donate" href="https://www.buymeacoffee.com/internetboy" target="_blank" rel="noopener noreferrer" ><FontAwesomeIcon className="socials-icn" icon={faPaypal} /></a>
+        <a aria-label="Support" data-tooltip="Donate" href="https://ko-fi.com/internetboy" target="_blank" rel="noopener noreferrer" ><FontAwesomeIcon className="socials-icn" icon={faPaypal} /></a>
       </div>
     </footer>
   </div>
